3b94c0a72cda674a803f9d2f9095e50a9509e3de
wietsebuseyne <wietsebuseyne@gmail.com>
2017-02-14 13:52:25 +0100

    Only adjusts the selected position when adding new events

--- library/src/main/java/com/alamkanak/weekview/WeekView.java
+++ library/src/main/java/com/alamkanak/weekview/WeekView.java
@@ -323,6 +323,7 @@ public class WeekView extends View {
                 mEventRects = new ArrayList<EventRect>();
                 if (selectedTime != null) {
                     if (mNewEventRect != null) {
+
                         tempEventRects.remove(mNewEventRect);
                         mNewEventRect = null;
                     }
@@ -1155,10 +1156,11 @@ public class WeekView extends View {
         int availableHeight = (int) (rect.bottom - originalTop - mEventPadding * 2);
         int availableWidth = (int) (rect.right - originalLeft - mEventPadding * 2);
 
-        // Get text dimensions.
+        // Get text color if necessary
         if(textColorPicker != null) {
             mEventTextPaint.setColor(textColorPicker.getTextColor(event));
         }
+        // Get text dimensions.
         StaticLayout textLayout = new StaticLayout(bob, mEventTextPaint, availableWidth, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
         if (textLayout.getLineCount() > 0) {
             int lineHeight = textLayout.getHeight() / textLayout.getLineCount();
